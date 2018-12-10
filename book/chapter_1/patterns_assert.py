cc06db246dc2fa9a13e6880bc7545d48aa9b1dd3
Colin <Colin_Paul@McAfee.com>
2018-12-10 13:27:06 +0000

    lunch-time python

--- book/chapter_1/patterns_assert.py
+++ book/chapter_1/patterns_assert.py
@@ -1,9 +1,15 @@
 def apply_discount(product, discount):
     price = int(product['price'] * (1.0 - discount))
-    assert 0 <= price <= product['price']
+    assert 0 <= price <= product['price'], "custom assert error"
     return price
 
 shoes = {'name': 'Fancy Shoes', 'price': 14900}
 
+# don't use assertions for data validation
+def delete_product(prod_id, user):
+    assert user.is_admin(), 'Must be admin'
+    assert store.has_product(prod_id), 'Unknown product'
+    store.get_product(prod_id).delete()
 
-# Continue from 2.1.3
+# instead use if statements
+# type updated code for delete_product here
