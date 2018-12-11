cf1dae85c5a8a9e35e5d3d09c95c90141747b379
Colin <Colin_Paul@McAfee.com>
2018-12-11 13:02:27 +0000

    just a few mins at lunch

--- book/chapter_1/patterns_assert.py
+++ book/chapter_1/patterns_assert.py
@@ -13,3 +13,12 @@ def delete_product(prod_id, user):
 
 # instead use if statements
 # type updated code for delete_product here
+def delete_product(product_id, user):
+    if not user.is_admin():
+        raise AuthError('Must be admin to delete')
+    if not store.has_product(product_id):
+        raise ValueError('Unknown product id')
+    store.get_product(product_id).delete()
+
+
+#continue from 2.2
